26fd785850f6161c5829209c6aab1600c5499c3a
Blake Batchelor <batchelorbh@vt.edu>
2024-09-29 22:52:01 -0400

    Fixed error in calculation

--- ymdhms_to_jd.py
+++ ymdhms_to_jd.py
@@ -29,20 +29,20 @@ import sys
 from math import floor
 
 #Pre-initialize input parameters
-year = float('nan') #Input year
-month = float('nan') #Input month
-day = float('nan') #Input day
-hour = float('nan') #Input hour
-minute = float('nan') #Input minute
+year = int(0) #Input year
+month = int(0) #Input month
+day = int(0) #Input day
+hour = int(0) #Input hour
+minute = int(0) #Input minute
 second = float('nan') #Input second
 
 #Arguments are strings by default
 if len(sys.argv) == 7:
-   year = float(sys.argv[1])
-   month = float(sys.argv[2])
-   day = float(sys.argv[3])
-   hour = float(sys.argv[4])
-   minute = float(sys.argv[5])
+   year = int(sys.argv[1])
+   month = int(sys.argv[2])
+   day = int(sys.argv[3])
+   hour = int(sys.argv[4])
+   minute = int(sys.argv[5])
    second = float(sys.argv[6])
 else:
    print('Usage: python3 ymdhms_to_jd.py year month day hour minute second')
@@ -50,14 +50,15 @@ else:
 
 #Main body of script
 jd1 = day - 32075
-jd2 = 1461 * (year + 4800 + (month - 14) / 12) / 4
-jd3 = 367 * (month - 2 - (month - 14) / 12 * 12) / 12
-jd4 = -3 * ((year + 4900 + (month - 14) / 12) / 100) / 4
+jd2 = 1461 * (year + 4800 + (month - 14) // 12) // 4
+jd3 = 367 * (month - 2 - (month - 14) // 12 * 12) // 12
+jd4 = -3 * ((year + 4900 + (month - 14) // 12) / 100) // 4
 
 jd = jd1 + jd2 + jd3 + jd4
 
 jd_mid = jd - 0.5
-d_frac = (second + 60 * (minute + 60 * hour)) / 86400
-jd_frac = "%.1f" % floor(jd_mid + d_frac)
+d_frac = (second + 60 * (minute + 60 * hour)) // 86400
+#jd_frac = "%.1f" % floor(jd_mid + d_frac)
+jd_frac = jd_mid + d_frac
 
 print(jd_frac)
